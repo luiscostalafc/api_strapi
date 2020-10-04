14749f67860dd2634f1047b561ed14c300a659e9
luiscostalafc <luiscostalafc@gmail.com>
2020-10-04 11:08:18 -0300

    Update configure

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a6c1fd0173065ef18cf58a5dd25fc2ab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e93b13b2-2a2e-4fec-80f9-7f883687729e'
+};
